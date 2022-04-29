7cf9b18834637a6a44964a1a218888e7daf4be3e
二月 <7889397+february_ge@user.noreply.gitee.com>
2022-04-29 10:49:21 +0800

    删除多余导包

--- src/main/java/com/eryue/paymentdemo/entity/OrderInfo.java
+++ src/main/java/com/eryue/paymentdemo/entity/OrderInfo.java
@@ -1,7 +1,5 @@
 package com.eryue.paymentdemo.entity;
 
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
