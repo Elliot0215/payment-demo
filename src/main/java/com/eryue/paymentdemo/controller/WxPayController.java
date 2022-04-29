17f053126dd556c5b49f8ee98aa1df4a5cdf4c59
二月 <7889397+february_ge@user.noreply.gitee.com>
2022-04-29 15:06:27 +0800

    增加支付通知接口

--- src/main/java/com/eryue/paymentdemo/controller/WxPayController.java
+++ src/main/java/com/eryue/paymentdemo/controller/WxPayController.java
@@ -2,12 +2,18 @@ package com.eryue.paymentdemo.controller;
 
 
 import com.eryue.paymentdemo.service.WxPayService;
+import com.eryue.paymentdemo.util.HttpUtils;
 import com.eryue.paymentdemo.vo.Result;
+import com.google.gson.Gson;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -16,7 +22,7 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 @Api(tags = "微信支付")
-@RequestMapping("/api/wxpay")
+@RequestMapping("/api/wx-pay")
 @Slf4j
 public class WxPayController {
 
@@ -26,8 +32,28 @@ public class WxPayController {
     @ApiOperation("生成支付二维码")
     @PostMapping("/native/{productId}")
     public Result nativePay(@PathVariable Long productId) throws Exception {
-        log.info("发起支付请求，入参productId:{}",productId);
-        Map<String,Object> resultMap = payService.nativePay(productId);
+        log.info("发起支付请求，入参productId:{}", productId);
+        Map<String, Object> resultMap = payService.nativePay(productId);
         return Result.ok().setData(resultMap);
+    }
+
+    /**
+     * 支付通知 * 微信支付通过支付通知接口将用户支付成功消息通知给商户
+     */
+    @ApiOperation("支付通知")
+    @PostMapping("/native/notify")
+    public String nativeNotify(HttpServletRequest request, HttpServletResponse response) {
+        Gson gson = new Gson();
+        Map<String, String> map = new HashMap<>();
+
+        String body = HttpUtils.readData(request);
+        Map<String, Object> bodyMap = gson.fromJson(body, HashMap.class);
+        log.info("支付通知的id ===> {}", bodyMap.get("id"));
+        log.info("支付通知的完整数据 ===> {}", body);
+
+        response.setStatus(200);
+        map.put("code", "SUCCESS");
+        map.put("message", "成功");
+        return gson.toJson(map);
     }
 }
